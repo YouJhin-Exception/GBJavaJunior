5481ca26776b17a83a5294bb8d95126ace8367f8
«YouJhin» <palevich86@mail.ru>
2023-12-06 14:40:13 +0300

    hw-2-tsk-1

---
+++ lesson2/src/main/java/ru/geekbrains/lesson2/hw2/Dog.java
@@ -0,0 +1,16 @@
+package src.main.java.ru.geekbrains.lesson2.hw2;
+
+public class Dog extends Animal implements CanMakeSound{
+
+
+    private int hiddenBones = 7;
+
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+}
